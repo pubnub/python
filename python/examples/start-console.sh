8395a9a3922e345608aa1e49b4aa27b9d5927a0d
Devendra <devendra@udgama.com>
2014-04-30 00:43:56 +0530

    making tweaks to run on osx

--- python/examples/start-console.sh
+++ python/examples/start-console.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if ! type "screen" > /dev/null; then
     echo "[ERROR] Screen is not installed. Please install screen to use this utility ."
     exit
@@ -5,4 +6,11 @@ fi
 rm ./pubnub-console.log
 touch ./pubnub-console.log
 set PYTHONPATH=../..
-screen -c config
+screen -X -S pubnub-console quit 2>&1 > /dev/null
+OS="`uname`"
+case $OS in
+  'darwin') 
+	screen -c config_osx
+    ;;
+  *) screen -c config;;
+esac

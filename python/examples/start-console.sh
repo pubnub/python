fc9ef45293540a2a5ff8f6dde5481442f4349f0c
Devendra <devendra@udgama.com>
2014-05-03 00:23:55 +0530

    adding virtualenv creation and installation of dependencies to start-console

--- python/examples/start-console.sh
+++ python/examples/start-console.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+#!/bin/bash -e
+
+BASEDIR=.
+
+if [ ! -d "$BASEDIR/ve" ]; then
+    virtualenv -q $BASEDIR/ve --no-site-packages
+    $BASEDIR/ve/bin/activate
+    echo "Virtualenv created."
+fi
+
+chmod 755 $BASEDIR/ve/bin/activate
+$BASEDIR/ve/bin/activate
+
+if [ ! -f "$BASEDIR/ve/updated" -o $BASEDIR/requirements.pip -nt $BASEDIR/ve/updated ]; then
+    pip install -r $BASEDIR/requirements.pip -E $BASEDIR/ve
+    touch $BASEDIR/ve/updated
+    echo "Requirements installed."
+fi
+
+
+
 if ! type "screen" > /dev/null; then
     echo "[ERROR] Screen is not installed. Please install screen to use this utility ."
     exit

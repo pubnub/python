09dbc7589b35963be1835ed58dcfd09d4d5c3ccc
Devendra <devendra@udgama.com>
2014-05-08 04:08:48 +0530

    adding packaging for pushing to pip

---
+++ python/examples/pubnub-console/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubnub-console',
+    version='3.5.0-beta',
+    description='PubNub Developer Console',
+    author='Stephen Blum',
+    author_email='support@pubnub.com',
+    url='http://pubnub.s3.amazonaws.com/pip/pubnub-3.3.5.tar.gz',
+    scripts=['pubnub-console'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=[
+        'pubnub==3.5.0-beta',
+        'cmd2>=0.6.7',
+    ],
+    zip_safe=False,
+)
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubnub',
+    version='3.5.0-beta',
+    description='PubNub Real-time push service in the cloud',
+    author='Stephen Blum',
+    author_email='support@pubnub.com',
+    url='http://pubnub.s3.amazonaws.com/pip/pubnub-3.3.5.tar.gz',
+    py_modules=['Pubnub'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=[
+        'pycrypto>=2.6.1',
+        'requests'
+    ],
+    zip_safe=False,
+)

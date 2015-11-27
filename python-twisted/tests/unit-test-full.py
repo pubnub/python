5b4ca0aa0cf198693e5c759a7d91b417195c81a8
Max Presman <max@presman.ca>
2015-11-27 14:58:25 -0800

    re-enable tests part 1

--- python-twisted/tests/unit-test-full.py
+++ python-twisted/tests/unit-test-full.py
@@ -1,21 +1,21 @@
-## www.pubnub.com - PubNub Real-time push service in the cloud.
+# www.pubnub.com - PubNub Real-time push service in the cloud.
 # coding=utf8
 
-## PubNub Real-time Push APIs and Notifications Framework
-## Copyright (c) 2010 Stephen Blum
-## http://www.pubnub.com/
+# PubNub Real-time Push APIs and Notifications Framework
+# Copyright (c) 2010 Stephen Blum
+# http://www.pubnub.com/
 
-## TODO Tests
-##
-## - wait 20 minutes, send a message, receive and success.
-## -
-## -
-##
-##
+# TODO Tests
+#
+# - wait 20 minutes, send a message, receive and success.
+# -
+# -
+#
+#
 
-## -----------------------------------
-## PubNub 3.1 Real-time Push Cloud API
-## -----------------------------------
+# -----------------------------------
+# PubNub 3.1 Real-time Push Cloud API
+# -----------------------------------
 
 import sys
 from pubnub import PubnubTwisted as Pubnub
@@ -26,9 +26,9 @@ secret_key = len(sys.argv) > 3 and sys.argv[3] or None
 cipher_key = len(sys.argv) > 4 and sys.argv[4] or None
 ssl_on = len(sys.argv) > 5 and bool(sys.argv[5]) or False
 
-## -----------------------------------------------------------------------
-## Command Line Options Supplied PubNub
-## -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# Command Line Options Supplied PubNub
+# -----------------------------------------------------------------------
 pubnub_user_supplied_options = Pubnub(
     publish_key,  # OPTIONAL (supply None to disable)
     subscribe_key,  # REQUIRED
@@ -37,20 +37,20 @@ pubnub_user_supplied_options = Pubnub(
     ssl_on  # OPTIONAL (supply None to disable)
 )
 
-## -----------------------------------------------------------------------
-## High Security PubNub
-## -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# High Security PubNub
+# -----------------------------------------------------------------------
 pubnub_high_security = Pubnub(
-    ## Publish Key
+    # Publish Key
     'pub-c-a30c030e-9f9c-408d-be89-d70b336ca7a0',
 
-    ## Subscribe Key
+    # Subscribe Key
     'sub-c-387c90f3-c018-11e1-98c9-a5220e0555fd',
 
-    ## Secret Key
+    # Secret Key
     'sec-c-MTliNDE0NTAtYjY4Ni00MDRkLTllYTItNDhiZGE0N2JlYzBl',
 
-    ## Cipher Key
+    # Cipher Key
     'YWxzamRmbVjFaa05HVnGFqZHM3NXRBS73jxmhVMkjiwVVXV1d5UrXR1JLSkZFRr' +
     'WVd4emFtUm1iR0TFpUZvbiBoYXMgYmVlbxWkhNaF3uUi8kM0YkJTEVlZYVFjBYi' +
     'jFkWFIxSkxTa1pGUjd874hjklaTFpUwRVuIFNob3VsZCB5UwRkxUR1J6YVhlQWa' +
@@ -62,13 +62,13 @@ pubnub_high_security = Pubnub(
     'VEExWdHVybiB0am0aW9uIb24gYXMgd2UgcG9zc2libHkgY2FuLuhcFe24ldWVns' +
     'dSaTFpU3hVUjFKNllWaFdhRmxZUWpCaQo34gcmVxdWlGFzIHNveqQl83snBfVl3',
 
-    ## 2048bit SSL ON - ENABLED TRUE
+    # 2048bit SSL ON - ENABLED TRUE
     True
 )
 
-## -----------------------------------------------------------------------
-## Channel | Message Test Data (UTF-8)
-## -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# Channel | Message Test Data (UTF-8)
+# -----------------------------------------------------------------------
 crazy = ' ~`â¦â§!@#$%^&*(顶顅Ȓ)+=[]\\{}|;\':",./<>?abcd'
 many_channels = [str(x) + '-many_channel_test' for x in range(10)]
 runthroughs = 0
@@ -76,9 +76,9 @@ planned_tests = 2
 delivery_retries = 0
 max_retries = 10
 
-## -----------------------------------------------------------------------
-## Unit Test Function
-## -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# Unit Test Function
+# -----------------------------------------------------------------------
 
 
 def test(trial, name):
@@ -91,9 +91,9 @@ def test(trial, name):
 def test_pubnub(pubnub):
     global runthroughs, planned_tests, delivery_retries, max_retries
 
-    ## -----------------------------------------------------------------------
-    ## Many Channels
-    ## -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # Many Channels
+    # -----------------------------------------------------------------------
     def phase2():
         status = {
             'sent': 0,
@@ -145,22 +145,22 @@ def test_pubnub(pubnub):
                 'callback': (lambda msg: received(msg, chan))
             })
 
-        ## Subscribe All Channels
+        # Subscribe All Channels
         for chan in many_channels:
             subscribe(chan)
 
-    ## -----------------------------------------------------------------------
-    ## Time Example
-    ## -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # Time Example
+    # -----------------------------------------------------------------------
     def time_complete(timetoken):
         test(timetoken, 'timetoken fetch')
         test(isinstance(timetoken, int), 'timetoken int type')
 
     pubnub.time({'callback': time_complete})
 
-    ## -----------------------------------------------------------------------
-    ## Publish Example
-    ## -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # Publish Example
+    # -----------------------------------------------------------------------
     def publish_complete(info):
         test(info, 'publish complete')
         test(info and len(info) > 2, 'publish response')
@@ -171,9 +171,9 @@ def test_pubnub(pubnub):
             'callback': history_complete
         })
 
-    ## -----------------------------------------------------------------------
-    ## History Example
-    ## -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # History Example
+    # -----------------------------------------------------------------------
     def history_complete(messages):
         test(messages and len(messages) > 0, 'history')
         test(messages, 'history')
@@ -184,9 +184,9 @@ def test_pubnub(pubnub):
         'callback': publish_complete
     })
 
-    ## -----------------------------------------------------------------------
-    ## Subscribe Example
-    ## -----------------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    # Subscribe Example
+    # -----------------------------------------------------------------------
     def message_received(message):
         test(message, 'message received')
         pubnub.unsubscribe({'channel': crazy})
@@ -222,9 +222,9 @@ def test_pubnub(pubnub):
 
     phase2()
 
-## -----------------------------------------------------------------------
-## Run Tests
-## -----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+# Run Tests
+# -----------------------------------------------------------------------
 test_pubnub(pubnub_user_supplied_options)
 test_pubnub(pubnub_high_security)
 pubnub_high_security.start()

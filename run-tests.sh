95354c766de5d6c26d722a8a859c9ac1b162752f
Alexander Novikov <anovikov@granpm.com>
2016-02-10 09:46:31 -0800

    Exclude 2.6/twisted env

---
+++ run-tests.sh
@@ -0,0 +1,10 @@
+nosetests python/tests/test_cg.py
+nosetests python/tests/test_grant.py
+nosetests python/tests/test_history.py
+
+if ! [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then
+  python python-twisted/tests/test_publish_async.py
+  python python-twisted/tests/test_grant_async.py
+fi
+
+flake8 --ignore=E501 .

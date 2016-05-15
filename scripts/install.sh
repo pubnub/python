171e21a7e77b296d1485a3fc8d4b035571d3dbf6
Alexander Novikov <alexander@pubnub.com>
2016-05-15 04:25:57 -0700

    Fix Python 2.6 missing mock

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then pip install -r requirements26.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then pip install twisted pyopenssl; fi

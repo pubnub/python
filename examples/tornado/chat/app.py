a834b8e06cf035041f8e1c788c7bb077479417af
Alexander Novikov <alexander@pubnub.com>
2016-05-19 12:09:34 -0700

    Add a base yield support to examples

--- examples/tornado/chat/app.py
+++ examples/tornado/chat/app.py
@@ -1,14 +1,17 @@
 import tornado.ioloop
 import tornado.web
+import tornado.gen
 import sys
 import os
+
+from tornado.stack_context import ExceptionStackContext
 
 d = os.path.dirname
 PUBNUB_ROOT = d(d(d(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(PUBNUB_ROOT)
 
 from pubnub.pnconfiguration import PNConfiguration
-from pubnub.pubnub import PubNub
+from pubnub.pubnub_tornado import PubNubTornado
 
 
 class AsyncHandler(tornado.web.RequestHandler):
@@ -17,7 +20,7 @@ class AsyncHandler(tornado.web.RequestHandler):
         pnconf = PNConfiguration()
         pnconf.subscribe_key = "demo"
         pnconf.publish_key = "demo"
-        pubnub = PubNub(pnconf)
+        pubnub = PubNubTornado(pnconf)
 
         pubnub.publish().channel("my_channel").message("hello").async(self.success, self.error)
 
@@ -30,9 +33,27 @@ class AsyncHandler(tornado.web.RequestHandler):
         self.finish()
 
 
+# TODO: implement yielding pubnub result
+class YieldHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        pnconf = PNConfiguration()
+        pnconf.subscribe_key = "demo"
+        pnconf.publish_key = "demo"
+        pubnub = PubNubTornado(pnconf)
+
+        def handle_err(type, second, third):
+            self.write(str(type))
+
+        with ExceptionStackContext(handle_err):
+            response = yield pubnub.publish().channel("my_channel").message("hello").deferred()
+            self.write(str(response))
+
+
 def make_app():
     return tornado.web.Application([
-        (r"/", AsyncHandler),
+        (r"/async", AsyncHandler),
+        (r"/yield", YieldHandler),
     ])
 
 if __name__ == "__main__":

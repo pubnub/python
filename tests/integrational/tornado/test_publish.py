740b51df3dd6ef2ca982b02deb693367d22988bd
Alexander Novikov <alexander@pubnub.com>
2016-05-24 01:49:37 -0700

    Fix publish tornado tests

--- tests/integrational/tornado/test_publish.py
+++ tests/integrational/tornado/test_publish.py
@@ -10,6 +10,42 @@ channel = "tornado-int-publish"
 
 class TestPubNubAsyncPublish(AsyncTestCase):
     def assert_success(self, pub):
+        self.pubnub.set_ioloop(self.io_loop)
+
+        def success(res):
+            self.pubnub.stop()
+            self.stop()
+            assert isinstance(res, PNPublishResult)
+            assert res.timetoken > 0
+            assert len(res.original_response) > 0
+
+        def error(err):
+            self.pubnub.stop()
+            self.stop()
+            self.fail("Error while success is expected: " + str(err))
+
+        pub.async(success, error)
+
+        self.pubnub.start()
+        self.wait()
+
+    def assert_success_publish_get(self, msg):
+        self.pubnub = PubNubTornado(pnconf)
+        self.assert_success(self.pubnub.publish().channel("my_channel").message(msg))
+
+    def assert_success_publish_post(self, msg):
+        self.pubnub = PubNubTornado(pnconf)
+        self.assert_success(self.pubnub.publish().channel("my_channel").message(msg).use_post(True))
+
+    def assert_success_publish_get_encrypted(self, msg):
+        self.pubnub = PubNubTornado(pnconf_enc)
+        self.assert_success(self.pubnub.publish().channel("my_channel").message(msg))
+
+    def assert_success_publish_post_encrypted(self, msg):
+        self.pubnub = PubNubTornado(pnconf_enc)
+        self.assert_success(self.pubnub.publish().channel("my_channel").message(msg).use_post(True))
+
+    def assert_error(self, pub):
         pubnub = PubNubTornado(pnconf)
         pubnub.set_ioloop(self.io_loop)
 
@@ -29,22 +65,6 @@ class TestPubNubAsyncPublish(AsyncTestCase):
 
         pubnub.start()
         self.wait()
-
-    def assert_success_publish_get(self, msg):
-        self.assert_success(
-            PubNubTornado(pnconf).publish().channel("my_channel").message(msg))
-
-    def assert_success_publish_post(self, msg):
-        self.assert_success(
-            PubNubTornado(pnconf).publish().channel("my_channel").message(msg).use_post(True))
-
-    def assert_success_publish_get_encrypted(self, msg):
-        self.assert_success(
-            PubNubTornado(pnconf_enc).publish().channel("my_channel").message(msg))
-
-    def assert_success_publish_post_encrypted(self, msg):
-        self.assert_success(
-            PubNubTornado(pnconf_enc).publish().channel("my_channel").message(msg).use_post(True))
 
     def test_publish_string_via_get(self):
         self.assert_success_publish_get("hi")
@@ -73,3 +93,4 @@ class TestPubNubAsyncPublish(AsyncTestCase):
         self.assert_success_publish_post_encrypted(True)
         self.assert_success_publish_post_encrypted(["hi", "hi2", "hi3"])
         self.assert_success_publish_post_encrypted({"name": "Alex", "online": True})
+

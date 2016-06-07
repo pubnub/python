16ee423fef42a6075201a5db37e1af0704d37cc9
Alexander Novikov <alexander@pubnub.com>
2016-06-07 09:08:54 -0700

    Fix tornado here_now test

--- tests/integrational/tornado/test_here_now.py
+++ tests/integrational/tornado/test_here_now.py
@@ -1,31 +1,22 @@
+import tornado
 from tornado.testing import AsyncHTTPTestCase, AsyncTestCase
-
-from pubnub.pnconfiguration import PNConfiguration
-
 from pubnub.pubnub_tornado import PubNubTornado
+from tests.helper import pnconf
 
 
-class TestPubNubAsyncAsyncHereNow(AsyncTestCase):
-
+class TestPubNubAsyncHereNow(AsyncTestCase):
+    @tornado.testing.gen_test
     def test_success(self):
-        pnconf = PNConfiguration()
         pubnub = PubNubTornado(pnconf)
         pubnub.set_ioloop(self.io_loop)
 
-        def success(res):
-            print(res.total_occupancy)
-            pubnub.stop()
-            self.stop()
-
-        def error(err):
-            print(err)
-            pubnub.stop()
-            self.stop()
-
-        pubnub.here_now() \
+        env = yield pubnub.here_now() \
             .channels(["ch1", "ch2", "ch3", "demo"]) \
             .include_state(False) \
-            .async(success, error)
+            .future()
 
-        pubnub.start()
-        self.wait()
+        print(env.result)
+
+        pubnub.stop()
+        self.stop()
+

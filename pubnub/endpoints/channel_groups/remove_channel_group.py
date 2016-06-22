985825a99fbbd5082c9c4936a8d9981dce323833
Alexander Novikov <alexander@pubnub.com>
2016-06-22 08:19:11 -0700

    Add RemoveChannelGroup method implementation with Tornado tests

---
+++ pubnub/endpoints/channel_groups/remove_channel_group.py
@@ -0,0 +1,46 @@
+import six
+
+from pubnub.endpoints.endpoint import Endpoint
+from pubnub.errors import PNERR_GROUP_MISSING
+from pubnub.exceptions import PubNubException
+from pubnub.enums import HttpMethod, PNOperationType
+
+
+class RemoveChannelGroup(Endpoint):
+    # /v1/channel-registration/sub-key/<sub_key>/channel-group/<group_name>/remove
+    REMOVE_PATH = "/v1/channel-registration/sub-key/%s/channel-group/%s/remove"
+
+    def __init__(self, pubnub):
+        Endpoint.__init__(self, pubnub)
+        self._channel_group = None
+
+    def channel_group(self, channel_group):
+        self._channel_group = channel_group
+
+        return self
+
+    def build_params(self):
+        return self.default_params()
+
+    def build_path(self):
+            return RemoveChannelGroup.REMOVE_PATH % (
+                self.pubnub.config.subscribe_key, self._channel_group)
+
+    def http_method(self):
+        return HttpMethod.GET
+
+    def validate_params(self):
+        self.validate_subscribe_key()
+
+        if not isinstance(self._channel_group, six.string_types)\
+                or len(self._channel_group) == 0:
+            raise PubNubException(pn_error=PNERR_GROUP_MISSING)
+
+    def create_response(self, envelope):
+        return envelope
+
+    def operation_type(self):
+        return PNOperationType.PNRemoveGroupOperation
+
+    def name(self):
+        return "RemoveChannelGroup"

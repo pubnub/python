3f744b76eeb3ff5e9895435801ddb19fbca0b69b
Alexander Novikov <alexander@pubnub.com>
2016-07-18 08:29:01 -0700

    Add missing tornado package in Python3.3 runner

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,6 @@
 pip install -r requirements-dev.txt
 if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then pip install -r requirements26-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then pip install -r requirements27-dev.txt; fi
+if [[ $TRAVIS_PYTHON_VERSION == 3.3 ]]; then pip install -r requirements33-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.4 ]]; then pip install -r requirements34-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.5 ]]; then pip install -r requirements35-dev.txt; fi

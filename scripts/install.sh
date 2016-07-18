c8ffaf5347e70d992ed62a7cfceb7d343e432e8e
Alexander Novikov <alexander@pubnub.com>
2016-07-18 02:29:32 -0700

    Update dev dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then pip install -r requirements26.txt; fi
-if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then pip install twisted pyopenssl; fi
+if [[ $TRAVIS_PYTHON_VERSION == 2.6 ]]; then pip install -r requirements26-dev.txt; fi
+if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then pip install -r requirements27-dev.txt; fi
+if [[ $TRAVIS_PYTHON_VERSION == 3.4 ]]; then pip install -r requirements34-dev.txt; fi
+if [[ $TRAVIS_PYTHON_VERSION == 3.5 ]]; then pip install -r requirements35-dev.txt; fi

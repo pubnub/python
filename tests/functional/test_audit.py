2cc0c43de60f57c86d1cddd6955c1475270e5716
Alexander Novikov <alexander@pubnub.com>
2016-07-23 10:57:44 -0700

    Add functional pam CG tests

--- tests/functional/test_audit.py
+++ tests/functional/test_audit.py
@@ -1,7 +1,7 @@
 import unittest
 
 from pubnub import utils
-from pubnub.endpoints.access.grant import Grant
+from pubnub.endpoints.access.audit import Audit
 
 try:
     from mock import MagicMock
@@ -12,7 +12,7 @@ from pubnub.pubnub import PubNub
 from tests.helper import pnconf_pam, sdk_name
 
 
-class TestGrant(unittest.TestCase):
+class TestAudit(unittest.TestCase):
     def setUp(self):
         self.pubnub = MagicMock(
             spec=PubNub,
@@ -21,26 +21,22 @@ class TestGrant(unittest.TestCase):
             timestamp=MagicMock(return_value=123),
             uuid=None
         )
-        self.pubnub.uuid = "UUID_GrantUnitTest"
-        self.grant = Grant(self.pubnub)
+        self.pubnub.uuid = "UUID_AuditUnitTest"
+        self.audit = Audit(self.pubnub)
 
-    def test_grant_read_and_write_to_channel(self):
-        self.grant.channels('ch').read(True).write(True)
+    def test_audit_channel(self):
+        self.audit.channels('ch')
 
-        self.assertEquals(self.grant.build_path(), Grant.GRANT_PATH % pnconf_pam.subscribe_key)
+        self.assertEquals(self.audit.build_path(), Audit.AUDIT_PATH % pnconf_pam.subscribe_key)
 
-        self.assertEqual(self.grant.build_params(), {
+        self.assertEqual(self.audit.build_params(), {
             'pnsdk': sdk_name,
             'uuid': self.pubnub.uuid,
-            'r': '1',
-            'w': '1',
             'timestamp': '123',
             'channel': 'ch',
             'signature': utils.sign_sha256(pnconf_pam.secret_key,
                                            pnconf_pam.subscribe_key + "\n" + pnconf_pam.publish_key + "\n" +
-                                           "grant\n" + utils.prepare_pam_arguments({
-                                               'r': '1',
-                                               'w': '1',
+                                           "audit\n" + utils.prepare_pam_arguments({
                                                'timestamp': 123,
                                                'channel': 'ch',
                                                'pnsdk': sdk_name,
@@ -48,23 +44,19 @@ class TestGrant(unittest.TestCase):
                                            }))
         })
 
-    def test_grant_read_and_write_to_channel_group(self):
-        self.grant.channel_groups(['gr1', 'gr2']).read(True).write(True)
+    def test_audit_channel_group(self):
+        self.audit.channel_groups(['gr1', 'gr2'])
 
-        self.assertEquals(self.grant.build_path(), Grant.GRANT_PATH % pnconf_pam.subscribe_key)
+        self.assertEquals(self.audit.build_path(), Audit.AUDIT_PATH % pnconf_pam.subscribe_key)
 
-        self.assertEqual(self.grant.build_params(), {
+        self.assertEqual(self.audit.build_params(), {
             'pnsdk': sdk_name,
             'uuid': self.pubnub.uuid,
-            'r': '1',
-            'w': '1',
             'timestamp': '123',
             'channel-group': 'gr1,gr2',
             'signature': utils.sign_sha256(pnconf_pam.secret_key,
                                            pnconf_pam.subscribe_key + "\n" + pnconf_pam.publish_key + "\n" +
-                                           "grant\n" + utils.prepare_pam_arguments({
-                                               'r': '1',
-                                               'w': '1',
+                                           "audit\n" + utils.prepare_pam_arguments({
                                                'timestamp': 123,
                                                'channel-group': 'gr1,gr2',
                                                'pnsdk': sdk_name,

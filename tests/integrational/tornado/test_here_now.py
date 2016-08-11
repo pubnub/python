7b38728b064eeff5c53fb095aba222ee8f335f1b
Alexander Novikov <alexander@pubnub.com>
2016-08-11 12:47:51 -0700

    Working on tornado here_now cassettes

--- tests/integrational/tornado/test_here_now.py
+++ tests/integrational/tornado/test_here_now.py
@@ -1,11 +1,17 @@
+import logging
 import tornado
+import tornado.gen
+import pubnub as pn
+
 from tornado import gen
 from tornado.testing import AsyncHTTPTestCase, AsyncTestCase
 
 from pubnub.pubnub_tornado import PubNubTornado
-from tests import helper
 from tests.helper import pnconf_sub_copy
 from tests.integrational.tornado.tornado_helper import connect_to_channel, disconnect_from_channel
+from tests.integrational.vcr_helper import use_cassette_and_stub_time_sleep
+
+pn.set_stream_logger('pubnub', logging.DEBUG)
 
 
 class TestPubNubAsyncHereNow(AsyncTestCase):
@@ -13,11 +19,15 @@ class TestPubNubAsyncHereNow(AsyncTestCase):
         super(TestPubNubAsyncHereNow, self).setUp()
         self.pubnub = PubNubTornado(pnconf_sub_copy(), custom_ioloop=self.io_loop)
 
+    @use_cassette_and_stub_time_sleep(
+        'tests/integrational/fixtures/tornado/here_now/single.yaml',
+        filter_query_parameters=['uuid', 'seqn'])
     @tornado.testing.gen_test(timeout=15)
-    def test_single_channel(self):
-        ch = helper.gen_channel("herenow-unit")
+    def test_here_now_single_channel(self):
+        ch = 'test-here-now-channel'
+        self.pubnub.config.uuid = 'test-here-now-uuid'
         yield connect_to_channel(self.pubnub, ch)
-        yield gen.sleep(4)
+        yield tornado.gen.sleep(10)
         env = yield self.pubnub.here_now() \
             .channels(ch) \
             .include_uuids(True) \
@@ -36,12 +46,22 @@ class TestPubNubAsyncHereNow(AsyncTestCase):
         self.pubnub.stop()
         self.stop()
 
-    @tornado.testing.gen_test(timeout=15)
-    def test_multiple_channels(self):
-        ch1 = helper.gen_channel("here-now")
-        ch2 = helper.gen_channel("here-now")
-        yield connect_to_channel(self.pubnub, [ch1, ch2])
-        yield gen.sleep(4)
+    # @use_cassette_and_stub_time_sleep(
+    #     'tests/integrational/fixtures/tornado/here_now/multiple.yaml',
+    #     filter_query_parameters=['uuid', 'tt', 'tr'])
+    @tornado.testing.gen_test(timeout=120)
+    def test_here_now_multiple_channels(self):
+        ch1 = 'test-here-now-channel1'
+        ch2 = 'test-here-now-channel2'
+        self.pubnub.config.uuid = 'test-here-now-uuid'
+        print("connecting to the first...")
+        yield connect_to_channel(self.pubnub, ch1)
+        print("...connected to the first")
+        yield gen.sleep(1)
+        print("connecting to the second...")
+        self.pubnub.subscribe().channels(ch2).execute()
+        print("...connected to the second")
+        yield gen.sleep(15)
         env = yield self.pubnub.here_now() \
             .channels([ch1, ch2]) \
             .future()
@@ -52,22 +72,26 @@ class TestPubNubAsyncHereNow(AsyncTestCase):
         channels = env.result.channels
 
         assert len(channels) == 2
-        assert channels[0].occupancy == 1
+        assert channels[0].occupancy >= 1
         assert channels[0].occupants[0].uuid == self.pubnub.uuid
-        assert channels[1].occupancy == 1
+        assert channels[1].occupancy >= 1
         assert channels[1].occupants[0].uuid == self.pubnub.uuid
 
         yield disconnect_from_channel(self.pubnub, [ch1, ch2])
         self.pubnub.stop()
         self.stop()
 
+    @use_cassette_and_stub_time_sleep(
+        'tests/integrational/fixtures/tornado/here_now/global.yaml',
+        filter_query_parameters=['uuid', 'seqn'])
     @tornado.testing.gen_test(timeout=15)
-    def test_global(self):
-        ch1 = helper.gen_channel("here-now")
-        ch2 = helper.gen_channel("here-now")
+    def test_here_now_global(self):
+        ch1 = 'test-here-now-channel1'
+        ch2 = 'test-here-now-channel2'
+        self.pubnub.config.uuid = 'test-here-now-uuid'
 
         yield connect_to_channel(self.pubnub, [ch1, ch2])
-        yield gen.sleep(4)
+        yield gen.sleep(6)
 
         env = yield self.pubnub.here_now().future()
 

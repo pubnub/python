c9a76dab1e837383c135091064503a8ddcf7652a
Alexander Novikov <alexander@pubnub.com>
2016-08-11 13:13:21 -0700

    Add asyncio state cassettes

--- tests/integrational/asyncio/test_state.py
+++ tests/integrational/asyncio/test_state.py
@@ -1,16 +1,20 @@
-import asyncio
-
 import pytest
 
 from pubnub.pubnub_asyncio import PubNubAsyncio
 from tests import helper
-from tests.helper import pnconf
+from tests.helper import pnconf, pnconf_copy
+from tests.integrational.vcr_helper import pn_vcr
 
 
+@pn_vcr.use_cassette(
+        'tests/integrational/fixtures/asyncio/state/single_channel.yaml',
+        filter_query_parameters=['uuid'],
+        match_on=['method', 'host', 'path', 'state_object_in_query'])
 @pytest.mark.asyncio
 def test_single_channel(event_loop):
-    pubnub = PubNubAsyncio(pnconf, custom_event_loop=event_loop)
-    ch = helper.gen_channel("herenow-unit")
+    pubnub = PubNubAsyncio(pnconf_copy(), custom_event_loop=event_loop)
+    ch = 'test-state-asyncio-ch'
+    pubnub.config.uuid = 'test-state-asyncio-uuid'
     state = {"name": "Alex", "count": 5}
 
     env = yield from pubnub.set_state() \
@@ -31,11 +35,16 @@ def test_single_channel(event_loop):
     pubnub.stop()
 
 
+@pn_vcr.use_cassette(
+        'tests/integrational/fixtures/asyncio/state/multiple_channel.yaml',
+        filter_query_parameters=['uuid'],
+        match_on=['method', 'host', 'path', 'state_object_in_query'])
 @pytest.mark.asyncio
 def test_multiple_channels(event_loop):
     pubnub = PubNubAsyncio(pnconf, custom_event_loop=event_loop)
-    ch1 = helper.gen_channel("herenow-unit")
-    ch2 = helper.gen_channel("herenow-unit")
+    ch1 = 'test-state-asyncio-ch1'
+    ch2 = 'test-state-asyncio-ch2'
+    pubnub.config.uuid = 'test-state-asyncio-uuid'
     state = {"name": "Alex", "count": 5}
 
     env = yield from pubnub.set_state() \

6708a3784395790b35590b24478c6869e042a777
Alexander Novikov <alexander@pubnub.com>
2016-08-18 03:09:40 -0700

    Update VCR heleprs

--- tests/integrational/vcr_helper.py
+++ tests/integrational/vcr_helper.py
@@ -121,6 +121,54 @@ def string_list_in_path_matcher(r1, r2, positions=None):
     return True
 
 
+def string_list_in_query_matcher(r1, r2, list_keys=None, filter_keys=None):
+    """
+    For here_now requests:
+
+    /v1/auth/grant/sub-key/my_sub_key?channel=ch1,ch2&timestamp=123
+    /v1/auth/grant/sub-key/my_sub_key?channel=ch2,ch1&timestamp=124
+
+    NOTICE: The :filter_query_parameters: cassette param should be specified alongside with :filter_keys:
+    """
+
+    if list_keys is None:
+        list_keys = []
+    elif isinstance(list_keys, six.string_types):
+        list_keys = [list_keys]
+
+    if filter_keys is None:
+        filter_keys = []
+    elif isinstance(filter_keys, six.string_types):
+        filter_keys = [filter_keys]
+
+    try:
+        list1 = r1.query
+        list2 = r2.query
+
+        for ik, tp in enumerate(list1):
+            k, v = tp
+            if k in filter_keys:
+                continue
+
+            if k in list_keys:
+                ary1 = v.split(',')
+                ary1.sort()
+                ary2 = list2[ik][1].split(',')
+                ary2.sort()
+
+                assert ary1 == ary2
+            else:
+                assert v == list2[ik][1]
+
+    except (AssertionError, IndexError) as e:
+        return False
+    except Exception as e:
+        print("Non-Assertion Exception: %s" % e)
+        raise
+
+    return True
+
+
 def check_the_difference_matcher(r1, r2):
     """ A helper to check the difference between two requests """
 
@@ -146,6 +194,7 @@ pn_vcr.register_matcher('object_in_path', object_in_path_matcher)
 pn_vcr.register_matcher('object_in_body', object_in_body_matcher)
 pn_vcr.register_matcher('check_the_difference', check_the_difference_matcher)
 pn_vcr.register_matcher('string_list_in_path', string_list_in_path_matcher)
+pn_vcr.register_matcher('string_list_in_query', string_list_in_query_matcher)
 
 
 def use_cassette_and_stub_time_sleep(cassette_name, **kwargs):
@@ -155,25 +204,25 @@ def use_cassette_and_stub_time_sleep(cassette_name, **kwargs):
     import tornado.gen
 
     @tornado.gen.coroutine
-    def tornado_returner():
+    def returner():
         return
 
     def _inner(f):
         @patch('time.sleep', return_value=None)
-        @patch('tornado.gen.sleep', return_value=tornado_returner())
+        @patch('tornado.gen.sleep', return_value=returner())
         @six.wraps(f)
         def stubbed(*args, **kwargs):
             with context as cassette:
                 largs = list(args)
-                # 0 - index
-                largs.pop(0)
-                largs.pop(0)
+                # 1 - index
+                largs.pop(1)
+                largs.pop(1)
                 return f(*largs, **kwargs)
 
         @six.wraps(f)
         def original(*args):
             with context as cassette:
-                yield from f(*args)
+                return f(*args)
 
         return stubbed if len(cs) > 0 else original
 
--- tests/unit/test_vcr_helper.py
+++ tests/unit/test_vcr_helper.py
@@ -1,14 +1,15 @@
 import unittest
 
-from tests.integrational.vcr_helper import string_list_in_path_matcher
+from tests.integrational.vcr_helper import string_list_in_path_matcher, string_list_in_query_matcher
 
 
 class Request(object):
-    def __init__(self, path):
+    def __init__(self, path=None, query=None):
         self.path = path
+        self.query = query
 
 
-class TestPreparePAMArguments(unittest.TestCase):
+class TestVCRMatchers(unittest.TestCase):
     def test_string_list_in_path_matcher(self):
         r1 = Request('/v2/presence/sub-key/my_sub_key/channel/ch1,ch2')
         r2 = Request('/v2/presence/sub-key/my_sub_key/channel/ch2,ch1')
@@ -20,3 +21,10 @@ class TestPreparePAMArguments(unittest.TestCase):
         assert not string_list_in_path_matcher(r2, r3, 6)
         assert string_list_in_path_matcher(r4, r5, 4)
         assert not string_list_in_path_matcher(r4, r5)
+
+    def test_string_list_in_path_query_matcher(self):
+        r1 = Request(query=[('channel', 'test-pam-asyncio-ch1,test-pam-asyncio-ch2'), ('pnsdk', 'PubNub-Python-Asyncio/4.0.0'), ('r', '1'), ('uuid', 'test-pam-asyncio-uuid'), ('w', '1')])
+        r2 = Request(query=[('channel', 'test-pam-asyncio-ch2,test-pam-asyncio-ch1'), ('pnsdk', 'PubNub-Python-Asyncio/4.0.0'), ('r', '1'), ('uuid', 'test-pam-asyncio-uuid'), ('w', '1')])
+
+        assert string_list_in_query_matcher(r1, r2, ['channel'])
+        assert not string_list_in_query_matcher(r1, r2)

5b0a0ffcc6ee06e9495ccb7022f3a181a127ab3b
Alexander Novikov <alexander@pubnub.com>
2016-09-05 14:14:32 -0700

    Fix typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,5 @@ if [[ $TRAVIS_PYTHON_VERSION == 3.3 ]]; then pip install -r requirements33-dev.t
 if [[ $TRAVIS_PYTHON_VERSION == 3.4.4 ]]; then pip install -r requirements34-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.5 ]]; then pip install -r requirements35-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.6 ]]; then pip install -r requirements36-dev.txt; fi
-if [[ $TRAVIS_PYTHON_VERSION == "nigthly" ]]; then pip install -r requirements36-dev.txt; fi
+if [[ $TRAVIS_PYTHON_VERSION == "nightly" ]]; then pip install -r requirements36-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == "pypy" ]]; then pip install -r requirements-pypy-dev.txt; fi

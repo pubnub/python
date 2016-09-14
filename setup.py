9ae61c089aade196e6f428823f1f3d6069f23feb
Alexander Novikov <alexander@pubnub.com>
2016-09-14 14:28:36 -0700

    Add missing setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pubnub',
+    version='4.0.0.beta1',
+    description='PubNub Real-time push service in the cloud',
+    author='PubNub',
+    author_email='support@pubnub.com',
+    url='http://pubnub.com',
+    py_modules=['pubnub'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=[],
+    zip_safe=False,
+)

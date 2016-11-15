19c2e84a6c6f89523756d4b87db718de3c449cd2
Max Presman <max@pubnub.com>
2016-11-15 15:00:39 -0800

    update pam signature

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -75,4 +75,4 @@ grant
 channel=asyncio-pam-FI2FCS0A&pnsdk=PubNub-Python-Asyncio%252F4.0.2&r=1&timestamp=1468409553&uuid=a4dbf92e-e5cb-428f-b6e6-35cce03500a2&w=1"""  # noqa: E501
         result = utils.sign_sha256("my_key", input)
 
-        assert "3P7WMijA_gF3mVRq4hUxmzmAl3sm4d_Hgpz2gQW9NrY=" == result
+        assert "ty5TgZtcl-wWkdNCbW--IHg_DPG7ryhfqxJnZhjmhD8=" == result

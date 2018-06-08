64129682d019b8e4bdcbad94d04edcccd728b5c0
QSD <info@qsdbih.com>
2018-06-08 13:07:47 +0200

    Resolve Travis conflicts by updating libs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -r requirements-dev.txt
-if [[ $TRAVIS_PYTHON_VERSION == 2.6* ]]; then pip install -r requirements26-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 2.7* ]]; then pip install -r requirements27-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.3* ]]; then pip install -r requirements33-dev.txt; fi
 if [[ $TRAVIS_PYTHON_VERSION == 3.4* ]]; then pip install -r requirements34-dev.txt; fi

6e3fa31b49e524bc6412e0885a5a0d7591a3d87a
650elx <bprokop02@gmail.com>
2021-11-15 21:20:48 +0100

    feat(PAM): implement remaining scenarios; enable them on GH actions.

--- tests/acceptance/pam/steps/when_steps.py
+++ tests/acceptance/pam/steps/when_steps.py
@@ -2,6 +2,7 @@ import json
 
 from behave import when
 import pubnub
+from pubnub.exceptions import PubNubException
 
 
 def execute_pam_call(context):
@@ -22,14 +23,41 @@ def step_impl(context):
     try:
         execute_pam_call(context)
     except pubnub.exceptions.PubNubException as err:
-        context.grant_call_error = json.loads(err._errormsg)
+        context.pam_call_error = json.loads(err._errormsg)
 
 
 @when("I parse the token")
 def step_impl(context):
-    context.parsed_token = context.peer.parse_token(context.token_to_parse)
+    context.parsed_token = context.peer.parse_token(context.token)
 
 
 @when("I grant a token specifying those permissions")
 def step_impl(context):
     execute_pam_call(context)
+
+
+@when("I publish a message using that auth token with channel {channel}")
+def step_impl(context, channel):
+    context.peer_without_secret_key.set_token(context.token)
+    context.publish_result = context.peer_without_secret_key.publish().channel(
+        channel.strip("'")
+    ).message("Tejjjjj").sync()
+
+
+@when("I attempt to publish a message using that auth token with channel {channel}")
+def step_impl(context, channel):
+    try:
+        context.peer_without_secret_key.set_token(context.token)
+        context.pam_call_result = context.peer_without_secret_key.publish().channel(
+            channel.strip("'")
+        ).message("Tejjjjj").sync()
+    except PubNubException as err:
+        context.pam_call_result = err
+
+
+@when("I revoke a token")
+def step_impl(context):
+    try:
+        context.revoke_result = context.peer.revoke_token(context.token).sync()
+    except PubNubException as err:
+        context.pam_call_error = json.loads(err._errormsg)

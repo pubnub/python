a546791cc6a23c21908c8d4d9a40d8a92c7da126
Sebastian Molenda <sebastian.molenda@pubnub.com>
2023-07-24 14:53:11 +0200

    Event engine  (#163)
    * Implementing subscribe loop
    
    * Handle messages
    
    * Handle messages
    
    * fix typing
    
    * Fixed tests

--- pubnub/event_engine/dispatcher.py
+++ pubnub/event_engine/dispatcher.py
@@ -1,21 +1,44 @@
-from pubnub.event_engine import effects
+from pubnub.event_engine.models import effects
+from pubnub.event_engine import manage_effects
 
 
 class Dispatcher:
-    def __init__(self) -> None:
+    _pubnub = None
+    _managed_effects_factory = None
+
+    def __init__(self, event_engine) -> None:
+        self._event_engine = event_engine
         self._managed_effects = {}
-        self._effect_emitter = effects.EmitEffect()
+        self._effect_emitter = manage_effects.EmitEffect()
+
+    def set_pn(self, pubnub_instance):
+        self._pubnub = pubnub_instance
+        self._effect_emitter.set_pn(pubnub_instance)
 
     def dispatch_effect(self, effect: effects.PNEffect):
+        print(f'dispatching {effect.__class__.__name__} {id(effect)}')
+        if not self._managed_effects_factory:
+            self._managed_effects_factory = manage_effects.ManagedEffectFactory(self._pubnub, self._event_engine)
+
         if isinstance(effect, effects.PNEmittableEffect):
-            self._effect_emitter.emit(effect)
+            self.emit_effect(effect)
 
-        if isinstance(effect, effects.PNManageableEffect):
-            managed_effect = effects.ManagedEffect(effect)
-            managed_effect.run()
-            self._managed_effects[effect.__class__.__name__] = managed_effect
+        elif isinstance(effect, effects.PNManageableEffect):
+            self.dispatch_managed_effect(effect)
 
-        if isinstance(effect, effects.PNCancelEffect):
-            if effect.cancel_effect in self._managed_effects:
-                self._managed_effects[effect.cancel_effect].stop()
-                del self._managed_effects[effect.cancel_effect]
+        elif isinstance(effect, effects.PNCancelEffect):
+            self.dispatch_cancel_effect(effect)
+
+    def emit_effect(self, effect: effects.PNEffect):
+        print(f'  emiting {effect.__class__.__name__} with {effect.__dict__}')
+        self._effect_emitter.emit(effect)
+
+    def dispatch_managed_effect(self, effect: effects.PNEffect):
+        managed_effect = self._managed_effects_factory.create(effect)
+        managed_effect.run()
+        self._managed_effects[effect.__class__.__name__] = managed_effect
+
+    def dispatch_cancel_effect(self, effect: effects.PNEffect):
+        if effect.cancel_effect in self._managed_effects:
+            self._managed_effects[effect.cancel_effect].stop()
+            del self._managed_effects[effect.cancel_effect]
--- pubnub/event_engine/statemachine.py
+++ pubnub/event_engine/statemachine.py
@@ -1,63 +1,99 @@
-from pubnub.event_engine import effects, events, states
+import logging
+
+from typing import List, Optional
+from queue import SimpleQueue
+
+from pubnub.event_engine.models import effects, events, states
 from pubnub.event_engine.dispatcher import Dispatcher
-from typing import List
 
 
 class StateMachine:
     _current_state: states.PNState
     _context: states.PNContext
     _effect_list: List[effects.PNEffect]
+    _enabled: bool
+    _effect_queue: SimpleQueue
 
-    def __init__(self, initial_state: states.PNState) -> None:
+    def __init__(self, initial_state: states.PNState, dispatcher_class: Optional[Dispatcher] = None) -> None:
         self._context = states.PNContext()
         self._current_state = initial_state(self._context)
         self._listeners = {}
         self._effect_list = []
-        self._dispatcher = Dispatcher()
+        if dispatcher_class is None:
+            dispatcher_class = Dispatcher
+        self._dispatcher = dispatcher_class(self)
+        self._enabled = True
 
     def get_state_name(self):
         return self._current_state.__class__.__name__
 
+    def get_context(self) -> states.PNContext:
+        return self._current_state._context
+
+    def get_dispatcher(self) -> Dispatcher:
+        return self._dispatcher
+
     def trigger(self, event: events.PNEvent) -> states.PNTransition:
+        logging.info(f'Triggered {event.__class__.__name__} on {self._current_state.__class__.__name__}')
+        if not self._enabled:
+            return False
         if event.get_name() in self._current_state._transitions:
+            self._effect_list.clear()
             effect = self._current_state.on_exit()
+            logging.info(f'On exit effect: {effect.__class__.__name__}')
+
             if effect:
                 self._effect_list.append(effect)
 
             transition: states.PNTransition = self._current_state.on(event, self._context)
 
             self._current_state = transition.state(self._current_state.get_context())
+
             self._context = transition.context
             if transition.effect:
-                self._effect_list.append(transition.effect)
+                if isinstance(transition.effect, list):
+                    logging.info('unpacking list')
+                    for effect in transition.effect:
+                        logging.info(f'Transition effect: {effect.__class__.__name__}')
+                        self._effect_list.append(effect)
+                else:
+                    logging.info(f'Transition effect: {transition.effect.__class__.__name__}')
+                    self._effect_list.append(transition.effect)
 
             effect = self._current_state.on_enter(self._context)
             if effect:
+                logging.info(f'On enter effect: {effect.__class__.__name__}')
                 self._effect_list.append(effect)
 
-            if transition.state:
-                self._current_state = transition.state(self._context)
-
         else:
+            self.stop()
             # we're ignoring events unhandled
-            print('unhandled event??')
+            logging.info(f'unhandled event?? {event.__class__.__name__} in {self._current_state.__class__.__name__}')
 
+        self.dispatch_effects()
+
+    def dispatch_effects(self):
         for effect in self._effect_list:
+            logging.info(f'dispatching {effect.__class__.__name__} {id(effect)}')
             self._dispatcher.dispatch_effect(effect)
 
-        return self._effect_list
+        self._effect_list.clear()
+
+    def stop(self):
+        self._enabled = False
 
 
+""" TODO: Remove before prodction """
 if __name__ == "__main__":
     machine = StateMachine(states.UnsubscribedState)
-    print(f'machine initialized. Current state: {machine.get_state_name()}')
+    logging.info(f'machine initialized. Current state: {machine.get_state_name()}')
     effect = machine.trigger(events.SubscriptionChangedEvent(
         channels=['fail'], groups=[]
     ))
 
-    machine.add_listener(effects.PNEffect, lambda x: print(f'Catch All Logger: {effect.__dict__}'))
+    machine.add_listener(effects.PNEffect, lambda x: logging.info(f'Catch All Logger: {effect.__dict__}'))
 
     machine.add_listener(effects.EmitMessagesEffect, )
     effect = machine.trigger(events.DisconnectEvent())
-    print(f'SubscriptionChangedEvent triggered with channels=[`fail`]. Current state: {machine.get_state_name()}')
-    print(f'effect queue: {machine._effect_list}')
+    logging.info(f'SubscriptionChangedEvent triggered with channels=[`fail`].  Curr state: {machine.get_state_name()}')
+    logging.info(f'effect queue: {machine._effect_list}')

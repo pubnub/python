701ece7e347da4db5165df81cc6f8a69377614e7
Sebastian Molenda <sebastian.molenda@pubnub.com>
2023-09-05 10:55:03 +0200

    Decouple and add configurability of crypto instance (#168)
    * Decouple and add configurability of crypto instance
    
    * Fix naming

--- pubnub/crypto_core.py
+++ pubnub/crypto_core.py
@@ -2,6 +2,9 @@ from abc import abstractmethod
 
 
 class PubNubCrypto:
+    def __init__(self, pubnub_config):
+        self.pubnub_configuration = pubnub_config
+
     @abstractmethod
     def encrypt(self, key, msg):
         pass

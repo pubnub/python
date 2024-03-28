a03a8167930c0d1276cd4a2327f4705ac0179f18
Sebastian Molenda <sebastian.molenda@pubnub.com>
2024-03-28 15:55:41 +0100

    Release of #183 (#184)
    * PubNub SDK v7.4.3 release.
    
    ---------
    
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pubnub',
-    version='7.4.2',
+    version='7.4.3',
     description='PubNub Real-time push service in the cloud',
     author='PubNub',
     author_email='support@pubnub.com',

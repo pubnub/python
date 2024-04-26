f56ef93f5b619e8780994ce1e4089173df489f20
Sebastian Molenda <sebastian.molenda@pubnub.com>
2024-04-26 15:13:05 +0200

    recommit matcher

--- pubnub/models/subscription.py
+++ pubnub/models/subscription.py
@@ -39,13 +39,8 @@ class PNEventEmitter:
 
         if self._type == PNSubscriptionType.CHANNEL:
             return message.channel == self.name or wildcard_match(message.channel, self.name)
-        elif self._type == PNSubscriptionType.CHANNEL_GROUP:
-            return message.subscription in self.name
         else:
-            return any(
-                [message.channel == subscription or wildcard_match(message.channel, subscription)
-                    for subscription in (self.channels + self.channel_groups)]
-            )
+            return message.subscription in self.name
 
     def presence(self, presence):
         if not hasattr(self, 'on_presence') or not self.with_presence:

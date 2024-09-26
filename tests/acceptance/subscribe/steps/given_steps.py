16a1f797842109669964e2a6979e253c84b53e7c
Sebastian Molenda <sebastian.molenda@pubnub.com>
2024-09-26 10:52:23 +0200

    aaaaaaaaaaaaaaasync

--- tests/acceptance/subscribe/steps/given_steps.py
+++ tests/acceptance/subscribe/steps/given_steps.py
@@ -1,6 +1,7 @@
 import logging
 
 from behave import given
+from behave.api.async_step import async_run_until_complete
 from io import StringIO
 from pubnub.enums import PNReconnectionPolicy
 from pubnub.pubnub_asyncio import PubNubAsyncio, EventEngineSubscriptionManager
@@ -9,7 +10,8 @@ from tests.acceptance.subscribe.environment import AcceptanceCallback, PNContext
 
 
 @given("the demo keyset with event engine enabled")
-def step_impl(context: PNContext):
+@async_run_until_complete
+async def step_impl(context: PNContext):
     context.log_stream = StringIO()
     logger = logging.getLogger('pubnub').getChild('subscribe')
     logger.setLevel(logging.DEBUG)
@@ -27,7 +29,8 @@ def step_impl(context: PNContext):
 
 
 @given("a linear reconnection policy with {max_retries} retries")
-def step_impl(context: PNContext, max_retries: str):
+@async_run_until_complete
+async def step_impl(context: PNContext, max_retries: str):
     context.pubnub.config.reconnect_policy = PNReconnectionPolicy.LINEAR
     context.pubnub.config.maximum_reconnection_retries = int(max_retries)
 
@@ -38,7 +41,8 @@ Presence engine step definitions
 
 
 @given("the demo keyset with Presence EE enabled")
-def step_impl(context: PNContext):
+@async_run_until_complete
+async def step_impl(context: PNContext):
     context.log_stream_pubnub = StringIO()
     logger = logging.getLogger('pubnub')
     logger.setLevel(logging.DEBUG)
@@ -66,6 +70,7 @@ def step_impl(context: PNContext):
 
 @given("heartbeatInterval set to '{interval}', timeout set to '{timeout}'"
        " and suppressLeaveEvents set to '{suppress_leave}'")
-def step_impl(context: PNContext, interval: str, timeout: str, suppress_leave: str):
+@async_run_until_complete
+async def step_impl(context: PNContext, interval: str, timeout: str, suppress_leave: str):
     context.pn_config.set_presence_timeout_with_custom_interval(int(timeout), int(interval))
     context.pn_config.suppress_leave_events = True if suppress_leave == 'true' else False

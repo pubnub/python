445a69542c8267c4d00335e55597bcfc51af939d
Sebastian Molenda <sebastian.molenda@pubnub.com>
2025-02-06 22:30:21 +0100

    Added tests and some fixes

--- tests/integrational/asyncio/objects_v2/test_channel.py
+++ tests/integrational/asyncio/objects_v2/test_channel.py
@@ -1,3 +1,4 @@
+import pytest
 from pubnub.endpoints.endpoint import Endpoint
 from pubnub.endpoints.objects_v2.channel.get_all_channels import GetAllChannels
 from pubnub.endpoints.objects_v2.channel.get_channel import GetChannel
@@ -8,15 +9,15 @@ from pubnub.models.consumer.common import PNStatus
 from pubnub.models.consumer.objects_v2.channel import PNSetChannelMetadataResult, PNGetChannelMetadataResult, \
     PNRemoveChannelMetadataResult, PNGetAllChannelMetadataResult
 from pubnub.models.consumer.objects_v2.sort import PNSortKey, PNSortKeyValue
-from pubnub.pubnub import PubNub
-from pubnub.structures import Envelope
+from pubnub.pubnub_asyncio import PubNubAsyncio
+from pubnub.models.envelopes import AsyncioEnvelope
 from tests.helper import pnconf_env_copy
 from tests.integrational.vcr_helper import pn_vcr
 
 
 def _pubnub():
     config = pnconf_env_copy()
-    return PubNub(config)
+    return PubNubAsyncio(config)
 
 
 class TestObjectsV2Channel:
@@ -41,20 +42,21 @@ class TestObjectsV2Channel:
         assert isinstance(set_channel, SetChannel)
         assert isinstance(set_channel, Endpoint)
 
-    @pn_vcr.use_cassette('tests/integrational/fixtures/native_sync/objects_v2/channel/set_channel.json',
-                         filter_query_parameters=['uuid', 'pnsdk'], serializer='pn_json')
-    def test_set_channel_happy_path(self):
+    @pn_vcr.use_cassette('tests/integrational/fixtures/asyncio/objects_v2/channel/set_channel.json',
+                         filter_query_parameters=['uuid', 'pnsdk', 'l_obj'], serializer='pn_json')
+    @pytest.mark.asyncio
+    async def test_set_channel_happy_path(self):
         pn = _pubnub()
 
-        set_channel_result = pn.set_channel_metadata() \
+        set_channel_result = await pn.set_channel_metadata() \
             .include_custom(True) \
             .channel(TestObjectsV2Channel._some_channel_id) \
             .set_name(TestObjectsV2Channel._some_name) \
             .description(TestObjectsV2Channel._some_description) \
             .custom(TestObjectsV2Channel._some_custom) \
-            .sync()
+            .future()
 
-        assert isinstance(set_channel_result, Envelope)
+        assert isinstance(set_channel_result, AsyncioEnvelope)
         assert isinstance(set_channel_result.result, PNSetChannelMetadataResult)
         assert isinstance(set_channel_result.status, PNStatus)
         assert not set_channel_result.status.is_error()
@@ -77,17 +79,18 @@ class TestObjectsV2Channel:
         assert isinstance(get_channel, GetChannel)
         assert isinstance(get_channel, Endpoint)
 
-    @pn_vcr.use_cassette('tests/integrational/fixtures/native_sync/objects_v2/channel/get_channel.json',
-                         filter_query_parameters=['uuid', 'pnsdk'], serializer='pn_json')
-    def test_get_channel_happy_path(self):
+    @pn_vcr.use_cassette('tests/integrational/fixtures/asyncio/objects_v2/channel/get_channel.json',
+                         filter_query_parameters=['uuid', 'pnsdk', 'l_obj'], serializer='pn_json')
+    @pytest.mark.asyncio
+    async def test_get_channel_happy_path(self):
         pn = _pubnub()
 
-        get_channel_result = pn.get_channel_metadata() \
+        get_channel_result = await pn.get_channel_metadata() \
             .include_custom(True) \
             .channel(TestObjectsV2Channel._some_channel_id) \
-            .sync()
+            .future()
 
-        assert isinstance(get_channel_result, Envelope)
+        assert isinstance(get_channel_result, AsyncioEnvelope)
         assert isinstance(get_channel_result.result, PNGetChannelMetadataResult)
         assert isinstance(get_channel_result.status, PNStatus)
         assert not get_channel_result.status.is_error()
@@ -110,16 +113,17 @@ class TestObjectsV2Channel:
         assert isinstance(remove_channel, RemoveChannel)
         assert isinstance(remove_channel, Endpoint)
 
-    @pn_vcr.use_cassette('tests/integrational/fixtures/native_sync/objects_v2/channel/remove_channel.json',
-                         filter_query_parameters=['uuid', 'pnsdk'], serializer='pn_json')
-    def test_remove_channel_happy_path(self):
+    @pn_vcr.use_cassette('tests/integrational/fixtures/asyncio/objects_v2/channel/remove_channel.json',
+                         filter_query_parameters=['uuid', 'pnsdk', 'l_obj'], serializer='pn_json')
+    @pytest.mark.asyncio
+    async def test_remove_channel_happy_path(self):
         pn = _pubnub()
 
-        remove_uid_result = pn.remove_channel_metadata() \
+        remove_uid_result = await pn.remove_channel_metadata() \
             .channel(TestObjectsV2Channel._some_channel_id) \
-            .sync()
+            .future()
 
-        assert isinstance(remove_uid_result, Envelope)
+        assert isinstance(remove_uid_result, AsyncioEnvelope)
         assert isinstance(remove_uid_result.result, PNRemoveChannelMetadataResult)
         assert isinstance(remove_uid_result.status, PNStatus)
         assert not remove_uid_result.status.is_error()
@@ -137,28 +141,29 @@ class TestObjectsV2Channel:
         assert isinstance(get_all_channel, GetAllChannels)
         assert isinstance(get_all_channel, Endpoint)
 
-    @pn_vcr.use_cassette('tests/integrational/fixtures/native_sync/objects_v2/channel/get_all_channel.json',
-                         filter_query_parameters=['uuid', 'pnsdk'], serializer='pn_json')
-    def test_get_all_channel_happy_path(self):
+    @pn_vcr.use_cassette('tests/integrational/fixtures/asyncio/objects_v2/channel/get_all_channel.json',
+                         filter_query_parameters=['uuid', 'pnsdk', 'l_obj'], serializer='pn_json')
+    @pytest.mark.asyncio
+    async def test_get_all_channel_happy_path(self):
         pn = _pubnub()
 
-        pn.set_channel_metadata() \
+        await pn.set_channel_metadata() \
             .include_custom(True) \
             .channel(TestObjectsV2Channel._some_channel_id) \
             .set_name(TestObjectsV2Channel._some_name) \
             .description(TestObjectsV2Channel._some_description) \
             .custom(TestObjectsV2Channel._some_custom) \
-            .sync()
+            .future()
 
-        get_all_channel_result = pn.get_all_channel_metadata() \
+        get_all_channel_result = await pn.get_all_channel_metadata() \
             .include_custom(True) \
             .limit(10) \
             .include_total_count(True) \
             .sort(PNSortKey.asc(PNSortKeyValue.ID), PNSortKey.desc(PNSortKeyValue.UPDATED)) \
             .page(None) \
-            .sync()
+            .future()
 
-        assert isinstance(get_all_channel_result, Envelope)
+        assert isinstance(get_all_channel_result, AsyncioEnvelope)
         assert isinstance(get_all_channel_result.result, PNGetAllChannelMetadataResult)
         assert isinstance(get_all_channel_result.status, PNStatus)
         assert not get_all_channel_result.status.is_error()
@@ -168,40 +173,43 @@ class TestObjectsV2Channel:
         assert get_all_channel_result.result.next is not None
         assert get_all_channel_result.result.prev is None
 
-    @pn_vcr.use_cassette('tests/integrational/fixtures/native_sync/objects_v2/channel/if_matches_etag.json',
-                         filter_query_parameters=['uuid', 'pnsdk'], serializer='pn_json')
-    def test_if_matches_etag(self):
-        config = pnconf_env_copy()
-        pubnub = PubNub(config)
+    @pn_vcr.use_cassette('tests/integrational/fixtures/asyncio/objects_v2/channel/if_matches_etag.json',
+                         filter_query_parameters=['uuid', 'pnsdk', 'l_obj'], serializer='pn_json')
+    @pytest.mark.asyncio
+    async def test_if_matches_etag(self):
+        pubnub = _pubnub()
 
-        set_channel = pubnub.set_channel_metadata(channel=self._some_channel_id, name=self._some_name).sync()
+        set_channel = await pubnub.set_channel_metadata(channel=self._some_channel_id, name=self._some_name).future()
         original_etag = set_channel.result.data.get('eTag')
-        get_channel = pubnub.get_channel_metadata(channel=self._some_channel_id).sync()
+        get_channel = await pubnub.get_channel_metadata(channel=self._some_channel_id).future()
         assert original_etag == get_channel.result.data.get('eTag')
 
         # Update without eTag should be possible
-        set_channel = pubnub.set_channel_metadata(channel=self._some_channel_id, name=f"{self._some_name}-2").sync()
+        set_channel = await pubnub.set_channel_metadata(channel=self._some_channel_id, name=f"{self._some_name}-2") \
+            .future()
 
         # Response should contain new eTag
         new_etag = set_channel.result.data.get('eTag')
         assert original_etag != new_etag
         assert set_channel.result.data.get('name') == f"{self._some_name}-2"
 
-        get_channel = pubnub.get_channel_metadata(channel=self._some_channel_id).sync()
+        get_channel = await pubnub.get_channel_metadata(channel=self._some_channel_id).future()
         assert original_etag != get_channel.result.data.get('eTag')
         assert get_channel.result.data.get('name') == f"{self._some_name}-2"
 
         # Update with correct eTag should be possible
-        set_channel = pubnub.set_channel_metadata(channel=self._some_channel_id, name=f"{self._some_name}-3") \
+        set_channel = await pubnub.set_channel_metadata(channel=self._some_channel_id, name=f"{self._some_name}-3") \
             .if_matches_etag(new_etag) \
-            .sync()
+            .future()
         assert set_channel.result.data.get('name') == f"{self._some_name}-3"
 
         try:
             # Update with original - now outdated - eTag should fail
-            set_channel = pubnub.set_channel_metadata(channel=self._some_channel_id, name=f"{self._some_name}-3") \
-                .if_matches_etag(original_etag) \
-                .sync()
+            set_channel = await pubnub.set_channel_metadata(
+                channel=self._some_channel_id,
+                name=f"{self._some_name}-3"
+            ).if_matches_etag(original_etag).future()
+
         except PubNubException as e:
             assert e.get_status_code() == 412
             assert e.get_error_message().get('message') == 'Channel to update has been modified after it was read.'

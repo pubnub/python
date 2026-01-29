5241c72269de36770b01379bc0705e0529237cfc
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2026-01-29 17:51:36 +0100

    Add optional parameters to PNConfiguration.__init__ (#228)
    * PubNub SDK 10.6.0 release.
    
    ---------
    
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pubnub',
-    version='10.5.0',
+    version='10.6.0',
     description='PubNub Real-time push service in the cloud',
     author='PubNub',
     author_email='support@pubnub.com',
